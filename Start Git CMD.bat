ff8ea88cfe6bcbbb4086fb4bfc5126bff74bee75
Joseph Shumway <thegreenjag1@gmail.com>
2017-07-08 23:37:43 -0500

    edited git push batch file

--- Start Git CMD.bat
+++ Start Git CMD.bat
@@ -21,9 +21,14 @@ if not "%pushOrCommit%" == "2" (
 goto choice
 )
 
+@echo.
 set /p id="Commit Message: "
+
 git add .
+
 set /p confirm="Commit these changes? Y/N: "
+@echo.
+
 if "%confirm%" == "y" (
 	git commit -m "%id%"
 	git push
